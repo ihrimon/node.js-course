5de36f261eea6cece0ea73e52184c1b802c5c772
ihrimon <ihrimon147@gmail.com>
2024-11-27 00:13:47 +0600

    async await function added

---
+++ 7.async-await/async-await.js
@@ -0,0 +1,19 @@
+// data fetching using async await function
+async function fetchAPI() {
+  const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+      return;
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('error message:', error.message);
+  }
+}
+
+fetchAPI();
